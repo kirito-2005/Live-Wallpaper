a26c109f6adbc98b85bde17e3a255c2267c40238
kirito-2005 <venkatkeer2014@gmail.com>
2025-12-01 12:13:22 +0530

    Initial Commit

---
+++ Live Wallpaper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Live Wallpaper
+//
+//  Created by Purushothaman venkatachalam on 01/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Live Wallpaper/Live_WallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_WallpaperApp.swift
+//  Live Wallpaper
+//
+//  Created by Purushothaman venkatachalam on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Live_WallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
